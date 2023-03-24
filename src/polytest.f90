981fc2452a51ad91c3f237e0e488c799bcca4bd0
jaiken17 <jaiken17@ubishops.ca>
2023-03-24 15:21:26 -0400

    added output of radial algorithm on test curve

--- src/polytest.f90
+++ src/polytest.f90
@@ -9,11 +9,12 @@ program PolyTest
     allocate(x(10))
 
     do i=1,10
-        x(i) = i
+        x(i) = 1.0_dp*i
     end do
     
     write(*,*) "x=", x
 
     write(*,*) "3th of x=", nthPoint(x,3)
+    write(*,*) "within 2 of x=", radialDistance(x,1._dp)
 
 end program PolyTest
