e96e2f570f01feb615b821010aa13f38604b99dc
jaiken17 <jaiken17@ubishops.ca>
2023-05-16 13:35:31 -0400

    added license blurb

--- src/polytest.f90
+++ src/polytest.f90
@@ -1,3 +1,10 @@
+!   Copyright (c) 2023, Joshua Aiken
+!   All rights reserved.
+!
+!   This source code is licensed under the BSD-style license found in the
+!   LICENSE file in the root directory of this source tree. 
+
+
 program poly_test
     use precision
     use simplify
