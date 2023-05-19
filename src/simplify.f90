fd54096c2407bc613998b75d8ade5687097c2bba
jaiken17 <jaiken17@ubishops.ca>
2023-05-19 17:58:31 -0400

    added overloaded functions to all but one, to return indices of original curve rather than data itsefl

--- src/simplify.f90
+++ src/simplify.f90
@@ -11,7 +11,8 @@ module simplify
 
 
     private
-    public nth_Point, radial_distance, perpendicular_distance, reumann_witkam
+    public nth_point, nth_point_indices, radial_distance, radial_distance_indices, perpendicular_distance,  &
+        perpendicular_distance_indices, reumann_witkam
 
 
     interface nth_point
@@ -19,15 +20,30 @@ module simplify
         procedure :: nth_point_single
     end interface nth_point
 
+    interface nth_point_indices
+        procedure :: nth_point_multi_indices
+        procedure :: nth_point_single_indices
+    end interface nth_point_indices
+
     interface radial_distance
         procedure :: radial_distance_multi
         procedure :: radial_distance_single
     end interface radial_distance
 
+    interface radial_distance_indices
+        procedure :: radial_distance_multi_indices
+        procedure :: radial_distance_single_indices
+    end interface radial_distance_indices
+
     interface perpendicular_distance
         procedure :: perp_distance
         procedure :: prependicular_distance_repeat
     end interface perpendicular_distance
+
+    interface perpendicular_distance_indices
+        procedure :: perp_distance_indices
+        !procedure :: perp_distance_repeat_indices
+    end interface perpendicular_distance_indices
 
 
     interface square_euclid_distance
@@ -203,6 +219,95 @@ contains
 
     end function nth_point_single
 
+
+
+    ! Index returning versions
+
+    function nth_point_multi_indices(curve,n) result(simple_curve_indices)
+
+        integer,dimension(:),allocatable :: simple_curve_indices
+        real(dp),dimension(:,:),intent(in) :: curve
+        integer,intent(in) :: n
+
+        integer :: i,length
+        integer :: counter,new_length
+
+        length = size(curve,dim=1)
+        
+        ! Make necessary checks
+        if (length < 2) then
+            stop 'curve is too short'
+        end if
+
+        if (n < 1) then
+            stop 'n is too small (<1)'
+        end if
+
+        ! Allocate maximum memory needed, will be trimmed at the end
+        allocate(simple_curve_indices(length))
+
+        simple_curve_indices(1) = 1   ! First element is always a key
+        counter = 1; new_length = 1;
+        do i=2,length-1
+            counter = counter + 1
+            if (counter == n) then
+                new_length = new_length + 1
+                simple_curve_indices(new_length) = i    ! add element as key
+                counter = 0 ! reset counter
+            end if
+        end do
+        new_length = new_length + 1
+        simple_curve_indices(new_length) = length  ! Last element is always a key
+        
+        simple_curve_indices = simple_curve_indices(1:new_length)    ! Resize final curve
+
+
+    end function nth_point_multi_indices
+
+    function nth_point_single_indices(curve,n) result(simple_curve_indices)
+
+        real(dp),dimension(:),allocatable :: simple_curve_indices
+        real(dp),dimension(:),intent(in) :: curve
+        integer,intent(in) :: n
+
+        integer :: i,length
+        integer :: counter,new_length
+
+        length = size(curve)
+
+        ! Make necessary checks
+        if (length < 2) then
+            stop 'curve is too short'
+        end if
+
+        if (n < 1) then
+            stop 'n is too small (<1)'
+        end if
+
+        ! Allocate maximum memory needed, will be trimmed at the end
+        allocate(simple_curve_indices(length))
+
+        simple_curve_indices(1) = 1   ! First element is always a key
+        counter = 1; new_length = 1;
+        do i=2,length-1
+            counter = counter + 1
+            if (counter == n) then
+                new_length = new_length + 1
+                simple_curve_indices(new_length) = i
+                counter = 0 ! reset counter
+            end if
+        end do
+
+        new_length = new_length + 1
+        simple_curve_indices(new_length) = length  ! Last element is always a key
+        
+        simple_curve_indices = simple_curve_indices(1:new_length)  ! Resize final curve
+
+
+    end function nth_point_single_indices
+
+
+
 ! ~~~~~~~ End Nth Point ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -322,6 +427,119 @@ contains
 
     end function radial_distance_single
 
+
+
+    ! Index returning versions
+
+
+    function radial_distance_multi_indices(curve,tolerance) result(simple_curve_indices)
+        real(dp),dimension(:),allocatable :: simple_curve_indices
+        real(dp),dimension(:,:),intent(in) :: curve
+        real(dp),intent(in) :: tolerance
+        
+        real(dp) :: square_tolerance
+        integer :: i, length
+        integer :: new_length
+        logical :: not_last
+        real(dp) :: distance
+        real(dp),dimension(:),allocatable :: current
+
+        ! Make necessary checks
+        if (tolerance <= 0._dp) then
+            stop 'tolerance must be >0'
+        end if
+
+        if (size(curve,dim=1) < 2) then
+            stop 'curve is too short'
+        end if
+
+        length = size(curve,dim=1)
+
+        ! Allocate absolute maximum memory needed, will be trimmed at end
+        allocate(simple_curve_indices(length))
+
+        square_tolerance = tolerance*tolerance
+
+        simple_curve_indices(1) = 1     ! First element is always a key
+        new_length = 1
+        i = 1
+        not_last = .true.
+        do while (not_last)
+            current = curve(simple_curve_indices(new_length),:)
+            i = i + 1
+            if (i==length) then
+                exit
+            end if
+            distance = square_euclid_distance(current,curve(i,:))
+            if (distance > square_tolerance) then
+                new_length = new_length + 1
+                simple_curve_indices(new_length) = i
+            end if
+        end do
+
+        new_length = new_length + 1
+        simple_curve_indices(new_length) = length  ! Last element is always a key
+
+        simple_curve_indices = simple_curve_indices(1:new_length)    ! Resize final curve
+
+    end function radial_distance_multi_indices
+
+    function radial_distance_single_indices(curve,tolerance) result(simple_curve_indices)
+        real(dp),dimension(:),allocatable :: simple_curve_indices
+        real(dp),dimension(:),intent(in) :: curve
+        real(dp),intent(in) :: tolerance
+        
+        real(dp) :: square_tolerance
+        integer :: i, length
+        integer :: new_length
+        logical :: not_last
+        real(dp) :: distance
+        real(dp) :: current
+
+        ! Make necessary checks
+
+        if (tolerance <= 0._dp) then
+            stop 'tolerance must be >0'
+        end if
+
+        if (size(curve,dim=1) < 2) then
+            stop 'curve is too short'
+        end if
+
+        length = size(curve,dim=1)
+
+
+        ! Allocate absolute maximum amount of memory needed, will be trimmed at end
+
+        allocate(simple_curve_indices(length))
+
+        square_tolerance = tolerance*tolerance
+
+        simple_curve_indices(1) = 1   ! First element is always a key
+        new_length = 1
+        i = 1
+        not_last = .true.
+        do while (not_last)
+            current = curve(simple_curve_indices(new_length))
+            i = i + 1
+            if (i==length) then
+                exit
+            end if
+            distance = square_euclid_distance(current,curve(i))
+            if (distance > square_tolerance) then
+                new_length = new_length + 1
+                simple_curve_indices(new_length) = i
+            end if
+        end do
+
+        new_length = new_length + 1
+        simple_curve_indices(new_length) = length  ! Last element is always a key
+
+        simple_curve_indices = simple_curve_indices(1:new_length)  ! Resize final curve
+
+    end function radial_distance_single_indices
+
+
 ! ~~~~~~~ End Radial Distance ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -418,6 +636,97 @@ contains
 
     end function prependicular_distance_repeat
 
+
+
+    ! Index returning versions
+
+
+function perp_distance_indices(curve,tolerance) result(simple_curve_indices)
+        real(dp),dimension(:),allocatable :: simple_curve_indices
+        real(dp),dimension(:,:),intent(in) :: curve
+        real(dp),intent(in) :: tolerance
+
+        integer :: i, length, new_length
+        logical :: last_is_key = .false., skip_iter = .false.
+        real(dp) :: square_distance, square_tolerance
+        
+
+        length = size(curve,dim=1)
+
+        ! Make necessary checks
+
+        if (tolerance <= 0._dp) then
+            stop 'tolerance must be >0'
+        end if
+
+        if (size(curve,dim=1) < 2) then
+            stop 'curve is too short'
+        end if
+
+        square_tolerance = tolerance*tolerance
+
+        ! Allocate absolute maximum amount of memory, will get trimmed at end
+
+        allocate(simple_curve_indices(length))
+
+        simple_curve_indices(1) = 1   ! First element is always a key
+        new_length = 1
+
+        do i=2,length-1
+            if (skip_iter) then
+                skip_iter = .false.
+                cycle
+            end if
+
+            square_distance = d2_line_to_point(curve(simple_curve_indices(new_length),:),curve(i+1,:),curve(i,:))
+            if (square_distance >= square_tolerance) then
+                new_length = new_length + 1
+                simple_curve_indices(new_length) = i
+            else
+                new_length = new_length + 1
+                simple_curve_indices(new_length) = i+1
+                skip_iter = .true.
+                if (i+1 == length) last_is_key = .true.
+            end if
+        end do
+
+        if (.not. last_is_key) then   ! last element may already be set as key
+            new_length = new_length + 1
+            simple_curve_indices(new_length) = length  ! Last element is always a key
+        end if
+
+        simple_curve_indices = simple_curve_indices(1:new_length)    ! Resize simple_curve to only needed elements
+
+    end function perp_distance_indices
+
+
+    ! Converting this procedure to return indices from original curve is non-trivial
+    ! For now, just call perp_distance_indices multiple times.
+
+    ! function prependicular_distance_repeat_indices(curve,tolerance,repeat) result(simple_curve_indices)
+    !     real(dp),dimension(:,:),allocatable :: simple_curve_indices
+    !     real(dp),dimension(:,:),intent(in) :: curve
+    !     real(dp),intent(in) :: tolerance
+    !     integer,intent(in) :: repeat
+
+    !     integer :: i
+        
+    !     if (repeat < 1) then
+    !         stop 'number of repeats too small'
+    !     end if
+
+    !     simple_curve = curve
+
+    !     do i=1,repeat
+    !         simple_curve = perp_distance(simple_curve,tolerance)
+    !     end do
+
+
+    ! end function prependicular_distance_repeat_indices
+
+
+
+
 ! ~~~~~~~ End Perpendicular Distan~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -504,6 +813,84 @@ contains
         simple_curve = simple_curve(1:new_length,:)    ! resize array
 
     end function reumann_witkam
+
+
+
+    ! Index returning version
+
+    function reumann_witkam_indices(curve,tolerance) result(simple_curve_indices)
+        real(dp),dimension(:),allocatable :: simple_curve_indices
+        real(dp),dimension(:,:),intent(in) :: curve
+        real(dp),intent(in) :: tolerance
+
+        integer :: i, j, length, new_length
+        logical :: not_last,not_key
+        real(dp) :: square_tolerance, square_distance
+        real(dp),dimension(:),allocatable :: current, next_point, test_point, next_potential_key
+        integer :: next_potential_key_index
+
+
+        length = size(curve,dim=1)
+
+        ! Make necessary checks
+
+        if (tolerance <= 0._dp) then
+            stop 'tolerance must be >0'
+        end if
+
+        if (length < 2) then
+            stop 'curve is too short'
+        end if
+
+        square_tolerance = tolerance*tolerance
+
+        ! allocate maximum amount of memory
+        allocate(simple_curve_indices(length))
+
+        simple_curve_indices(1) = 1   ! First element is always a key
+        new_length = 1
+
+        i = 1
+        not_last = .true.
+
+        main_loop: do while(not_last)
+            i = i + 1
+            if (i==length) then ! last point is always a key
+                new_length = new_length + 1
+                simple_curve_indices(new_length) = i
+                exit main_loop
+            end if
+            current = curve(simple_curve_indices(new_length),:)  ! current is most recent key
+            next_point = curve(i,:)             ! next point is point next to most recent key
+            next_potential_key = next_point
+            next_potential_key_index = i
+            not_key = .true.
+            j = i + 1
+            do while(not_key)   ! loop over points until one is outside of tolerance. key is the last within tolerance
+                test_point = curve(j,:)
+                square_distance = d2_line_to_point(current,next_point,test_point)
+                if (square_distance >= square_tolerance) then     ! test point is outside of tolerance so previous point is key
+                    new_length = new_length + 1
+                    simple_curve_indices(new_length) = next_potential_key_index
+                    not_key = .false.
+                else if (j == length) then  ! last point is always a key
+                    new_length = new_length + 1
+                    simple_curve_indices(new_length) = j
+                    exit main_loop
+                else            ! test point still within tolerance so is upgraded to next potential key
+                    next_potential_key = test_point
+                    next_potential_key_index = j
+                    j = j + 1
+                end if
+            end do
+            i = j-1
+
+        end do main_loop
+
+
+        simple_curve_indices = simple_curve_indices(1:new_length)    ! resize array
+
+    end function reumann_witkam_indices
 
 
 
